4d39e03b7912b334aae17faac689611a2978bae5
Ayiks <cliffysarp@gmail.com>
2024-06-21 16:56:22 +0000

    title change

--- lib/app/modules/onboarding/views/select_category_view.dart
+++ lib/app/modules/onboarding/views/select_category_view.dart
@@ -17,26 +17,62 @@ class SelectCategoryView extends GetView<SelectCategoryController> {
           title: HeadingText(
             text: 'Define your role',
           ),
-          centerTitle: true,
+          centerTitle: false,
         ),
-        body: SingleChildScrollView(
-          padding: EdgeInsets.only(left: 20, top: 20, right: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              HeadingText(text: 'Individual'),
-              Stack(children: [
-                Container(
-                  height: 269,
-                  decoration: const BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage('assets/catImage.png')),
-                      // color: Colors.black,
-                      borderRadius: BorderRadius.all(Radius.circular(50))),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            padding: EdgeInsets.only(left: 20, top: 20, right: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                HeadingText(
+                  text: 'Individual',
+                  size: 20,
                 ),
-                GestureDetector(
-                  onTap: () => Get.to(Routes.USER_LOGIN),
-                  child: Positioned(
+                Stack(children: [
+                  Container(
+                    height: 269,
+                    decoration: const BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage('assets/catImage.png')),
+                        // color: Colors.black,
+                        borderRadius: BorderRadius.all(Radius.circular(50))),
+                  ),
+                  Positioned(
+                      bottom: 35,
+                      right: 20,
+                      child: GestureDetector(
+                        onTap: () => Get.toNamed(Routes.USER_LOGIN),
+                        child: Container(
+                          height: 44,
+                          width: 65,
+                          decoration: BoxDecoration(
+                              color: AppColors.buttonColor,
+                              borderRadius: BorderRadius.circular(10)),
+                          child: const Icon(
+                            Icons.chevron_right,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ))
+                ]),
+                const SizedBox(
+                  height: 30,
+                ),
+                HeadingText(
+                  text: 'Service Provider',
+                  size: 20,
+                ),
+                Stack(children: [
+                  Container(
+                    height: 269,
+                    decoration: const BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage('assets/catImage.png')),
+                        // color: Colors.black,
+                        borderRadius: BorderRadius.all(Radius.circular(50))),
+                  ),
+                  Positioned(
                       bottom: 35,
                       right: 20,
                       child: Container(
@@ -49,38 +85,10 @@ class SelectCategoryView extends GetView<SelectCategoryController> {
                           Icons.chevron_right,
                           color: Colors.white,
                         ),
-                      )),
-                )
-              ]),
-              SizedBox(
-                height: 30,
-              ),
-              HeadingText(text: 'Service Provider'),
-              Stack(children: [
-                Container(
-                  height: 269,
-                  decoration: const BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage('assets/catImage.png')),
-                      // color: Colors.black,
-                      borderRadius: BorderRadius.all(Radius.circular(50))),
-                ),
-                Positioned(
-                    bottom: 35,
-                    right: 20,
-                    child: Container(
-                      height: 44,
-                      width: 65,
-                      decoration: BoxDecoration(
-                          color: AppColors.buttonColor,
-                          borderRadius: BorderRadius.circular(10)),
-                      child: const Icon(
-                        Icons.chevron_right,
-                        color: Colors.white,
-                      ),
-                    ))
-              ])
-            ],
+                      ))
+                ])
+              ],
+            ),
           ),
         ));
   }
