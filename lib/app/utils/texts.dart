7a3c8d02fb67bfc8d65a18272200cc8dc571d9e0
Ayiks <cliffysarp@gmail.com>
2024-06-21 16:56:09 +0000

    dynamic size

--- lib/app/utils/texts.dart
+++ lib/app/utils/texts.dart
@@ -2,14 +2,17 @@ import 'package:flutter/material.dart';
 
 class HeadingText extends StatelessWidget {
   final String text;
-  HeadingText({super.key, required this.text});
+  double? size;
+  HeadingText({super.key, required this.text, this.size});
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
-      style: const TextStyle(
-          color: Colors.black, fontSize: 25, fontWeight: FontWeight.w600),
+      style: TextStyle(
+          color: Colors.black,
+          fontSize: size ?? 25,
+          fontWeight: FontWeight.w600),
     );
   }
 }
